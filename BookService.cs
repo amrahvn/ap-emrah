e8a82fdde8d52e198d280d70b24743f09f893b83
Amrahvn <amrahvn@code.edu.az>
2023-04-13 04:11:43 +0400

    add new file

---
+++ BookService.cs
@@ -0,0 +1,36 @@
+﻿using BookApp1.Core.Models;
+
+namespace BookApp1.App.Services
+{
+    internal class BookService
+    {
+        private Book[] books = new Book[0];
+
+        public void GetFullInfo()
+        {
+
+            if (books.Length == 0)
+            {
+                Console.WriteLine("book not included");
+            }
+            foreach (Book books in books)
+            {
+                books.GetFullInfo();
+            }
+        }
+
+              public void Createbooks()
+            {
+                Book book = new Book();
+
+                Console.WriteLine("Please, enter a book name");
+                book.Name = Console.ReadLine();
+                Console.WriteLine("Please, enter the price of the book");
+                book.Price = double.Parse(Console.ReadLine());
+
+                Array.Resize(ref books, books.Length + 1);
+                books[books.Length - 1] = book;
+              }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using BookApp1.App.Services;
+
+BookService Book = new BookService();
+
+
+Console.WriteLine("0.Close app Please");
+Console.WriteLine("1.Show book Please");
+Console.WriteLine("2.Create book Please");
+
+string aplication=Console.ReadLine();
+
+
+while (aplication != "0")
+{
+    switch (aplication)
+    {
+        case "1":
+            Book.GetFullInfo();
+            break;
+            case "2":
+            Book.Createbooks();
+            break;
+
+        default:
+            Console.WriteLine("If you don't mind, please enter right away");
+            break;
+
+    }
+    Console.WriteLine("0.Close app Please");
+    Console.WriteLine("1.Show book Please");
+    Console.WriteLine("2.Create book Please");
+
+    aplication = Console.ReadLine();
+}
